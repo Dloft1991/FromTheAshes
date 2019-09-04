c709b8423d2b87e5463926b1c3f5ae27f37eaa43
Dloft <derek.lofton1@gmail.com>
2019-09-04 11:36:34 -0400

    js

---
+++ JS/Men.js
@@ -0,0 +1,20 @@
+// icon functions
+// $("#Cart").on("click", function() {
+//     alert("Added to your Cart.");
+// });
+// $("#Heart").on("click", function() {
+//     alert("You Loved.");
+// });
+// $("#Share").on("click", function() {
+//     alert("You can now share this.");
+// });
+
+function Cart() {
+    alert("Added to Cart.");
+  };
+  function Heart() {
+    alert("You Loved this.");
+  };
+  function Share() {
+    alert("You can now share this.");
+  };
