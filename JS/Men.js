d5bf6163c7383dec1bbf69964a1fa56c05ef86f6
Dloft <derek.lofton1@gmail.com>
2020-01-17 10:59:25 -0500

    margins/New

--- JS/Men.js
+++ JS/Men.js
@@ -1,26 +1,31 @@
 
-RedHeart = document.getElementById("RedHeart").hidden = true;
-// function Cart() {
-//     alert("Added to Cart.");
+// RedHeart = document.getElementById("RedHeart").hidden = true;
+// // function Cart() {
+// //     alert("Added to Cart.");
+// //   };
+//   function Heart() {
+//     // alert("You Loved this.");
+//     Like();
 //   };
-  function Heart() {
-    // alert("You Loved this.");
-    Like();
-  };
-  function UnHeart() {
-    // alert("No longer Love.");
-    UnLike();
-  };
-//   function Share() {
-//     alert("You can now share this.");
+//   function UnHeart() {
+//     // alert("No longer Love.");
+//     UnLike();
 //   };
+// //   function Share() {
+// //     alert("You can now share this.");
+// //   };
 
-  function Like() {
-      document.getElementById("Heart").hidden = true;
-      document.getElementById("RedHeart").hidden = false;
-  }
-  function UnLike() {
-    document.getElementById("Heart").hidden = false;
-    document.getElementById("RedHeart").hidden = true;
-}
+//   function Like() {
+//       document.getElementById("Heart").hidden = true;
+//       document.getElementById("RedHeart").hidden = false;
+//   }
+//   function UnLike() {
+//     document.getElementById("Heart").hidden = false;
+//     document.getElementById("RedHeart").hidden = true;
+// }
   
+// AddCart = document.getElementById("cart");
+
+// function Cart() {
+//   alert("added to Cart.");
+// } 
